8c19ff977e1f348d41a375f00cf52be4876962da
Kyle Brandon Sioson <32427049+Kyliekyler@users.noreply.github.com>
2021-02-25 05:47:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,88 +26,17 @@ try:
     apiid = config("APP_ID", cast=int)
     apihash = config("API_HASH")
     bottoken = config("BOT_TOKEN")
-
-
-    datgbot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
-
-
-@datgbot.on(events.NewMessage(incoming=True, chats=-1001412651756)) 
-async def _(event): 
-    if not event.is_private:
-        try:
-            await event.client.send_message("-1001311556665, event.message)
-        except:
-            print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
-
-
-print("Bot has started.")
-datgbot.run_until_disconnected()
-#    This file is part of the ChannelAutoForwarder distribution (https://github.com/xditya/ChannelAutoForwarder).
-
-#    Copyright (c) 2021 Adiya
-
-#    
-
-#    This program is free software: you can redistribute it and/or modify  
-
-#    it under the terms of the GNU General Public License as published by  
-
-#    the Free Software Foundation, version 3.
-
-# 
-
-#    This program is distributed in the hope that it will be useful, but 
-
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-
-#    General Public License for more details.
-
-# 
-
-#    License can be found in < https://github.com/xditya/ChannelAutoForwarder/blob/main/License> .
-
-import logging
-
-import asyncio
-
-from telethon import TelegramClient, events, Button
-
-from decouple import config
-
-from telethon.tl.functions.users import GetFullUserRequest
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
-
-# start the bot
-
-print("Starting...")
-
-try:
-
-    apiid = config("APP_ID", cast=int)
-
-    apihash = config("API_HASH")
-
-    bottoken = config("BOT_TOKEN")
-
     suryabot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
 
+
 @suryabot.on(events.NewMessage(incoming=True, chats="-1001412651756")) 
-
 async def _(event): 
-
     if not event.is_private:
-
         try:
-
             await event.client.send_message("-1001311556665", event.message)
-
         except:
-
             print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
 
-print("Bot has started.")
 
+print("Bot has started.")
 suryabot.run_until_disconnected()
