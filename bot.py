cf166bda795849d70f00744aaf1d3bc2778905ab
Kyle Brandon Sioson <32427049+Kyliekyler@users.noreply.github.com>
2021-02-25 07:00:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,34 @@
 import logging
 import asyncio
 from telethon import TelegramClient, events, Button
-from os import environ
+from decouple import config
 from telethon.tl.functions.users import GetFullUserRequest
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
 
 # start the bot
 print("Starting...")
-apiid = int(environ.get("APP_ID", None))
-apihash = environ.get("API_HASH", None)
-bottoken = environ.get("BOT_TOKEN", None)
-suryabot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
+try:
+    apiid = config("APP_ID", cast=int)
+    apihash = config("API_HASH")
+    bottoken = config("BOT_TOKEN")
+    frm = config("FROM_CHANNEL", cast=int)
+    tochnl = config("TO_CHANNEL", cast=int)
+    suryabot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
+except:
+    print("Environment vars are missing! Kindly recheck.")
+    print("Bot is quiting...")
+    exit()
 
 
-@suryabot.on(events.NewMessage(incoming=True, chats=-1001412651756)) 
+@suryabot.on(events.NewMessage(incoming=True, chats=frm)) 
 async def _(event): 
     if not event.is_private:
         try:
-            await event.client.send_message(-1001311556665, event.message)
+            await event.client.send_message(tochnl, event.message)
         except:
             print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
 
 
 print("Bot has started.")
+suryabot.run_until_disconnected()
