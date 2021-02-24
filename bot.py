bcd9d03cc21d5c330be8953b8bd0b0578ac93feb
Kyle Brandon Sioson <32427049+Kyliekyler@users.noreply.github.com>
2021-02-25 06:17:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,3 @@
-#    This file is part of the ChannelAutoForwarder distribution (https://github.com/xditya/ChannelAutoForwarder).
-#    Copyright (c) 2021 Adiya
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/xditya/ChannelAutoForwarder/blob/main/License> .
-
 import logging
 import asyncio
 from telethon import TelegramClient, events, Button
@@ -29,11 +15,11 @@ try:
     suryabot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
 
 
-@suryabot.on(events.NewMessage(incoming=True, chats="-1001412651756")) 
+@suryabot.on(events.NewMessage(incoming=True, chats=-1001412651756)) 
 async def _(event): 
     if not event.is_private:
         try:
-            await event.client.send_message("-1001311556665", event.message)
+            await event.client.send_message(-1001311556665, event.message)
         except:
             print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
 
