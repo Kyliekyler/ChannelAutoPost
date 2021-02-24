f5f7be13fd6d19cf59d0e3a4123016f903d5993e
Kyle Brandon Sioson <32427049+Kyliekyler@users.noreply.github.com>
2021-02-25 06:32:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,21 +8,19 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 # start the bot
 print("Starting...")
-try:
-    apiid = config("APP_ID", cast=int)
-    apihash = config("API_HASH")
-    bottoken = config("BOT_TOKEN")
-    suryabot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
+apiid = config("APP_ID", cast=int)
+apihash = config("API_HASH")
+bottoken = config("BOT_TOKEN")
+suryabot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
 
 
-@suryabot.on(events.NewMessage(incoming=True, chats=-1412651756)) 
+@suryabot.on(events.NewMessage(incoming=True, chats=-1001412651756)) 
 async def _(event): 
     if not event.is_private:
         try:
-            await event.client.send_message(-1311556665, event.message)
+            await event.client.send_message(-1001311556665, event.message)
         except:
             print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
 
 
 print("Bot has started.")
-suryabot.run_until_disconnected()
